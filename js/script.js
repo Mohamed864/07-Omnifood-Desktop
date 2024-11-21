674d7dd05a6bd4dd219f95d238778b3efd95cda5
Mohamed864 <beherm884@gmail.com>
2024-11-21 19:00:22 +0200

    done js

--- js/script.js
+++ js/script.js
@@ -31,7 +31,59 @@ btnNavEl.addEventListener("click", function () {
 // fix scroll and make it smooth
 
 const allLinks = document.querySelectorAll("a:link");
-con;
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    //scroll back top
+
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    // scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+
+    //close mobile nav da lma yro7 a2flha 3n tree2 toggle
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+///////////////////////////////////////////////////////////
+// stick navbar
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
+// test and optimize to other browsers
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
